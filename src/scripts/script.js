2caf72461b3335c309a3ca27d57ffe0406ceda49
Guilherme <guilhermebugs2001@gmail.com>
2023-04-01 18:19:54 -0300

    Finizalização primeira entrega

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,36 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+
+function openModal() {
+
+    let activeModal = document.querySelectorAll('.active-modal')
+    let modal = document.getElementById("container-modal")
+
+    activeModal.forEach(buttonActive => {
+        buttonActive.addEventListener('click', () => {
+            modal.showModal();
+            closeModal()
+        })
+    })
+
+}
+
+function closeModal() {
+    let closeModal = document.querySelector("#close-modal")
+    let modal = document.getElementById("container-modal")
+
+    closeModal.addEventListener('click', () => {
+        modal.close();
+    })
+}
+
+function aboutUS() {
+    let buttonAboutUs = document.querySelector(".container-first__button")
+
+    buttonAboutUs.addEventListener('click', () => {
+        window.scroll(0, 700);
+    })
+}
+
+
+openModal()
+aboutUS()
